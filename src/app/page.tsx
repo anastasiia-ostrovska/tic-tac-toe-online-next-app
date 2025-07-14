6a57f99a9220e1232a842b9f5e73d58c152c329d
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-07-14 18:22:30 +0300

    feat: add db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import {Button} from "@/shared/ui/button";
+import { Button } from "@/shared/ui/button";
+import { prisma } from "@/shared/lib/db";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <Button size='lg'>Button</Button>
-    </div>
-  );
+export default async function Home() {
+	const games = await prisma.game.findMany();
+
+	return (
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+			<Button size="lg">{games[0].name}</Button>
+			<Button size="lg">{games[1].name}</Button>
+		</div>
+	);
 }
