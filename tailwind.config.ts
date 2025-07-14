54046177a36cf72eb8c0eba20009ccd8b9d51182
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-07-14 13:49:04 +0300

    config: change eslint and font configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,8 @@
 export default {
-    darkMode: ["class"],
-    content: ["./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-} ;
+	darkMode: ["class"],
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
