5a88c070163fa5ceb38209b0a4a3c187728b7df9
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-07-15 18:01:05 +0300

    feat: add a games-list; add game entity;

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,24 +1,36 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const emptyField = Array(9).fill(null);
 
 async function main() {
+	const user = await prisma.user.create({
+		data: { login: "Ann", passwordHash: "gfg8377fgff", rating: 1000 },
+	});
+	const user2 = await prisma.user.create({
+		data: { login: "John", passwordHash: "suwujl77fgff", rating: 950 },
+	});
 	await prisma.game.create({
 		data: {
-			name: 'game-1'
-		}
-	})
+			field: emptyField,
+			status: "gameIdle",
+			players: { connect: { id: user.id } },
+		},
+	});
 	await prisma.game.create({
 		data: {
-			name: 'game-2'
-		}
-	})
+			field: emptyField,
+			status: "gameIdle",
+			players: { connect: { id: user2.id } },
+		},
+	});
 }
 main()
 	.then(async () => {
-		await prisma.$disconnect()
+		await prisma.$disconnect();
 	})
 	.catch(async (e) => {
-		console.error(e)
-		await prisma.$disconnect()
-		process.exit(1)
-	})
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
