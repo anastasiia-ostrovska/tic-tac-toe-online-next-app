e80e46bbeb725b7cd42057f267ab23e38ed0574f
anastasiia-ostrovska <anastasiyakov99@gmail.com>
2025-08-02 12:48:11 +0300

    feat: add password hashing;

---
+++ src/entities/user/services/passwords.ts
@@ -0,0 +1,31 @@
+import { pbkdf2, randomBytes } from "node:crypto";
+
+export const passwordService = { hashPassword, comparePasswords };
+
+async function hashPassword(
+	password: string,
+	salt = randomBytes(16).toString("hex")
+) {
+	const hash = await new Promise<Buffer>((resolve, reject) => {
+		pbkdf2(password, salt, 1000, 64, `sha512`, (error, hash) => {
+			error ? reject(error) : resolve(hash);
+		});
+	});
+
+	return {
+		hash: hash.toString(`hex`),
+		salt,
+	};
+}
+
+async function comparePasswords({
+	hash,
+	password,
+	salt,
+}: {
+	hash: string;
+	password: string;
+	salt: string;
+}) {
+	return hash === (await hashPassword(password, salt)).hash;
+}
