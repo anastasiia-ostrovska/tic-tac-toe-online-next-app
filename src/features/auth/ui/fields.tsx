6eff0bf2bdafb7e055cf26b6e4b3ddc8f171918e
Anastasiia Ostrovska <95317301+anastasiia-ostrovska@users.noreply.github.com>
2025-09-12 20:28:49 +0300

    fix: form default values (#6)
    * fix: forms default values and validation;

--- src/features/auth/ui/fields.tsx
+++ src/features/auth/ui/fields.tsx
@@ -2,7 +2,13 @@ import { useId } from "react";
 import { Input } from "@/shared/ui/input";
 import { Label } from "@/shared/ui/label";
 
-export function AuthFormFields() {
+export function AuthFormFields({
+	formData,
+	errors,
+}: {
+	formData?: FormData;
+	errors?: { login?: string; password?: string };
+}) {
 	const loginId = useId();
 	const passwordId = useId();
 
@@ -11,22 +17,30 @@ export function AuthFormFields() {
 			<div className="space-y-2">
 				<Label htmlFor="email">Email</Label>
 				<Input
+					required
 					id={loginId}
 					type="text"
 					name="login"
 					placeholder="Enter your email"
-					required
+					defaultValue={formData?.get("login")?.toString()}
 				/>
+				{errors?.login && (
+					<p className="text-sm text-red-500">{errors.login}</p>
+				)}
 			</div>
 			<div className="space-y-2">
 				<Label htmlFor="password">Password</Label>
 				<Input
+					required
 					id={passwordId}
 					type="password"
 					name="password"
 					placeholder="Enter your password"
-					required
+					defaultValue={formData?.get("password")?.toString()}
 				/>
+				{errors?.password && (
+					<p className="text-sm text-red-500">{errors.password}</p>
+				)}
 			</div>
 		</>
 	);
